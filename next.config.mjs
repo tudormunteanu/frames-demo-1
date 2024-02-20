6d1ea8240eeb39d5b414cdffddb08544fefc12c4
tudormunteanu <tudor@boto.io>
2024-02-20 08:09:12 -0800

    Undo nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 // next.config.js
-const nextConfig = {
-  webpack: (cfg, options = {}) => {
-    cfg.externals.push('sharp')
-    const { webpack } = options
-    const regex = /^sharp$/
-    cfg.plugins.push(new webpack.IgnorePlugin({
-      resourceRegExp: regex,
-    }))
-  }
-}
+const nextConfig = {}
 
 export default nextConfig;
