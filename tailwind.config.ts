2bd3f173e4300b4ac4e2a4e50676729b4ffd235c
tudormunteanu <tudor@boto.io>
2024-02-20 08:21:53 -0800

    Add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
-};
-export default config;
+} satisfies Config;
