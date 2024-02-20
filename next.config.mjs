cc9ad5e8d1a086c4be99793648f1d40de6e88fe8
tudormunteanu <tudor@boto.io>
2024-02-20 08:02:27 -0800

    Add sharp back and update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+  webpack: (cfg, options = {}) => {
+    cfg.externals.push('sharp')
+    const { webpack } = options
+    const regex = /^sharp$/
+    cfg.plugins.push(new webpack.IgnorePlugin({
+      resourceRegExp: regex,
+    }))
+  }
+}
 
 export default nextConfig;
